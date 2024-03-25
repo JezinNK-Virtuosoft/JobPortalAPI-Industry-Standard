76c0fc8dda755bba8068d31874dbb8f25a7e76cf
JezinNK-Virtuosoft <162125878+JezinNK-Virtuosoft@users.noreply.github.com>
2024-03-25 10:54:09 +0530

    changed the email format

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -39,15 +39,39 @@ namespace JobPortalAPI_1.Repository
             await Task.Run(() =>
             {
                 string Subject = "Welcome Aboard! Verify Your Email Address for Full Access to Job Portal";
-                string Message = $"Dear {MailContent["Name"]},\r\n\r\nWelcome to JobPortal! We're thrilled to have you on board. To ensure the security of your account, we've generated a temporary password for you to sign in. Here are your temporary login details:\r\n\r\nUsername/Email:{MailContent["Email"]} \r\nTemporary Password: {MailContent[MailContent["Email"]]}" +
-                     "\r\nPlease sign in using the provided credentials to access your account. Once signed in, we highly recommend that you update your password to something more memorable and secure. You can change your password by following these simple steps:\r\n\r\n" +
-                     "1.Sign in to your account using the temporary credentials provided above.\r\n" +
-                     "2.Navigate to your account settings or profile settings.\r\n" +
-                     "3.Select the option to change your password.\r\n" +
-                     "4.Enter your current temporary password and create a new, secure password.\r\n" +
-                     "5.Save your changes.\r\nRemember, choosing a strong password is crucial for the security of your account. " +
-                     "Ensure your new password is unique, includes a mix of letters, numbers, and special characters, and is not easily guessable." +
-                     "Thank you for choosing Job Portal. We look forward to assisting you in your job search journey.\r\n\r\nBest regards,";
+                string Message = $@"<html>
+                                    <body>
+                                     <p>Dear {MailContent["Name"]},</p>
+                                     <p>Welcome to JobPortal! We're thrilled to have you on board. To ensure the security of your account, we've generated a  temporary password for you to sign in.</p>
+                                     <p>Here are your temporary login details:</p>
+                                      <ul>
+                                            <li>Username/Email:{MailContent["Email"]}</li>
+                                            <li>Temporary Password: {MailContent[MailContent["Email"]]}</li>
+                                      </ul>
+                                       <p>Please sign in using the provided credentials to access your account. Once signed in, we highly recommend that you update your password to something more memorable and secure.</p>
+                                       <p>You can change your password by following these simple steps:</p>
+                                        <ol>
+                                            <li>Sign in to your account using the temporary credentials provided above.</li>
+                                            <li>Navigate to your account settings or profile settings.</li>
+                                            <li>Select the option to change your password.</li>
+                                            <li>Enter your current temporary password and create a new, secure password.</li>
+                                            <li>Save your changes.</li>
+                                        </ol>
+                                        <p>Remember, choosing a strong password is crucial for the security of your account.</p>
+                                        <p>Ensure your new password is unique, includes a mix of letters, numbers, and special characters, and is not easily guessable.</p>
+                                        <p>Thank you for choosing Job Portal. We look forward to assisting you in your job search journey.</p>
+                                        <p>Best regards,</p>
+                                    </body>   
+                                    </html>";
+                     //Dear {MailContent["Name"]},\r\n\r\nWelcome to JobPortal! We're thrilled to have you on board. To ensure the security of your account, we've generated a temporary password for you to sign in. Here are your temporary login details:\r\n\r\nUsername/Email:{MailContent["Email"]} \r\nTemporary Password: {MailContent[MailContent["Email"]]}" +
+                     //"\r\nPlease sign in using the provided credentials to access your account. Once signed in, we highly recommend that you update your password to something more memorable and secure. You can change your password by following these simple steps:\r\n\r\n" +
+                     //"1.Sign in to your account using the temporary credentials provided above.\r\n" +
+                     //"2.Navigate to your account settings or profile settings.\r\n" +
+                     //"3.Select the option to change your password.\r\n" +
+                     //"4.Enter your current temporary password and create a new, secure password.\r\n" +
+                     //"5.Save your changes.\r\nRemember, choosing a strong password is crucial for the security of your account. " +
+                     //"Ensure your new password is unique, includes a mix of letters, numbers, and special characters, and is not easily guessable." +
+                     //"Thank you for choosing Job Portal. We look forward to assisting you in your job search journey.\r\n\r\nBest regards,";
                 EmailContent.Add("Subject", Subject);
                 EmailContent.Add("Message", Message);
                 EmailContent.Add("Email", MailContent["Email"]);
